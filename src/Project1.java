9ddb915660e339f9a6cf7323bf959fd2e7c22d7a
EdsonFufu <91458499+EdsonFufu@users.noreply.github.com>
2021-12-31 23:41:47 +0300

    Add getMenu static function display two operation view files and file operation and then creating swich case and get user input then list operation of view files base on user selection

--- src/Project1.java
+++ src/Project1.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Project1 {
     public static void main(String[] args) {
 
+    }
+    private static void getMenu() {
+        System.out.println("1: View Files\n2: File Operations\n0: Exit");
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            Integer in = input.nextInt();
+            switch (in) {
+                case 1:
+                    retrieve();
+                    break;
+                case 2:
+                    fileOperations();
+                    break;
+                case 0:
+                    System.exit(1);
+                    break;
+                default:
+                    System.out.println("Invalid Input, Try [1,2] or 0 to exit");
+            }
+        }
+    }
+
+    private static void fileOperations() {
+    }
+
+    private static void retrieve() {
     }
 }
