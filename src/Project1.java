43955c7f6a177bf46e383f2b15ce9865c3e16a99
EdsonFufu <91458499+EdsonFufu@users.noreply.github.com>
2022-01-01 02:58:30 +0300

    add copyFile function and in main add function to check if directory main is empty then copy files from .idea directory

--- src/Project1.java
+++ src/Project1.java
@@ -1,8 +1,42 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Project1 {
-    public static void main(String[] args) {
+    static String PATH2MAIN = "main";
+    static String PATH2DOTIDEA = ".idea";
 
+    public static void main(String[] args) throws IOException {
+        File directory = new File(PATH2MAIN);
+        //Check if main directory exists and create if not
+        if (! directory.exists()){
+            directory.mkdirs();
+        }
+        //check if directory id empty and copy files
+        if(directory.list().length < 1){
+            System.out.println("Copying Files .....................");
+            copyFile(PATH2DOTIDEA,PATH2MAIN);
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("=================== Welcome to the  Project1 ========================");
+        System.out.println("!!!!!!!!! To continue type 1 to start using application or 0 to exit !!!!!!!!!");
+        while (true) {
+            int n = input.nextInt();
+            if (n == 1) {
+                getMenu();
+            } else if (n == 0) {
+                System.out.println("Bye!!!");
+                System.exit(1);
+            } else {
+                System.out.println("Invalid Input, try again [0,1]");
+            }
+        }
     }
     private static void getMenu() {
         System.out.println("1: View Files\n2: File Operations\n0: Exit");
@@ -11,7 +45,7 @@ public class Project1 {
             Integer in = input.nextInt();
             switch (in) {
                 case 1:
-                    retrieve();
+                    viewFiles();
                     break;
                 case 2:
                     fileOperations();
@@ -26,8 +60,17 @@ public class Project1 {
     }
 
     private static void fileOperations() {
+
     }
 
-    private static void retrieve() {
+    private static void viewFiles() {
+        Arrays.stream(new File(PATH2MAIN).listFiles()).forEach(System.out::println);
+        getMenu();
     }
+    public static void copyFile(String from, String to) throws IOException {
+        Path src = Paths.get(from);
+        Path dest = Paths.get(to);
+        Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+    }
+
 }
